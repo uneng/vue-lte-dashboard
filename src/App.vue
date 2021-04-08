77918f35e4e9a1b71afa472532b80ef93ca3d0c7
uneng <mutar1005@gmail.com>
2021-04-09 07:50:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <firafa />
+  </div>
+</template>
+
+<script>
+import Firafa from './components/Firafa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Firafa
+  }
+}
+</script>
